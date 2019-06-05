df8a78c9440ac8e6f9d6cc3a04334fa3ca87f66c
chetan882777 <chetan882777@gmail.com>
2019-06-06 01:41:06 +0530

    inserting more into db

--- mongodb.js
+++ mongodb.js
@@ -5,16 +5,48 @@ const connUrl = 'mongodb://127.0.0.1:27017'
 const databasename = "task-manager"
 
 MongodbClient.connect(connUrl, { useNewUrlParser: true }, (error, client) => {
-    if(error){
+    if (error) {
         return console.log('unable to connect to database')
     }
 
 
     const db = client.db(databasename)
 
-    db.collection('users').insertOne({
-        name: 'chetan',
-        age : 20
+    // db.collection('users').insertMany([{
+    //         name: 'rounak',
+    //         age: 20
+    //     },
+    //     {
+    //         name: 'sawan',
+    //         age: 20
+    //     }
+    // ], (error, result) => {
+    //     if(error){
+    //         return console.log('Unable to insert Documents!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+
+    db.collection('tasks').insertMany([{
+            description: 'Take 6 hour sleep',
+            complete: false
+        },
+        {
+            description: 'Read lesson 2 of English Litrature',
+            complete: false
+        },
+        {
+            description: 'Practice coding',
+            complete: true
+        },
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert Documents!')
+        }
+
+        console.log(result.ops)
     })
 
 })
