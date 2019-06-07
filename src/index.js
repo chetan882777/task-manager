6b76609b65421d2568aad6d349ad2289e94a05ca
chetan882777 <chetan882777@gmail.com>
2019-06-08 02:23:53 +0530

    Resource reading endpoint

--- src/index.js
+++ src/index.js
@@ -28,6 +28,50 @@ app.post('/tasks', (req, res) => {
     })
 })
 
+app.get('/users' , (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+app.get('/users/:id' , (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+
+
+app.get('/tasks' , (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+app.get('/tasks/:id' , (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
 app.listen(port , () => {
     console.log('Server is up on port ' + port)
 })
