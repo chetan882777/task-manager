1915de8847cea83eddd75990328a6b70ac28de48
chetan882777 <chetan882777@gmail.com>
2019-06-07 20:23:40 +0530

    validate data with mongoose 2

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -45,34 +45,36 @@ const User = mongoose.model('user', {
     }
 })
 
-const me = new User({
-     name: '  Chetan   ',
-     email: '  chetan@gmail.com',
-     password: ' 123456'
-})
-
-me.save().then(()=>{
-    console.log(me)
-}).catch((error)=>{
-    console.log(error)
-})
-
-// const Task = mongoose.model('tasks', {
-//     description:{
-//         type: String
-//     },
-//     completed: {
-//         type: Boolean
-//     }
+// const me = new User({
+//      name: '  Chetan   ',
+//      email: '  chetan@gmail.com',
+//      password: ' 123456'
 // })
 
-// const myTask = new Task({
-//      description: 'Start coding' , 
-//      completed: false
-// })
-
-// myTask.save().then(()=>{
-//     console.log(myTask)
+// me.save().then(()=>{
+//     console.log(me)
 // }).catch((error)=>{
 //     console.log(error)
 // })
+
+const Task = mongoose.model('tasks', {
+    description:{
+        type: String,
+        required:true,
+        trim: true,
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const myTask = new Task({
+     description: '    Start coding' , 
+})
+
+myTask.save().then(()=>{
+    console.log(myTask)
+}).catch((error)=>{
+    console.log(error)
+})
