b0536400515c5e7f1f39ec952993d6eb8c28c413
chetan882777 <chetan882777@gmail.com>
2019-06-08 02:03:33 +0530

    add status code to each response

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((e) => {
         res.status(400).send(e)
     })
@@ -22,7 +22,7 @@ app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
     task.save().then(() => {
-        res.send(task)
+        res.status(201).send(task)
     }).catch((e) => {
         res.status(400).send(e)
     })
