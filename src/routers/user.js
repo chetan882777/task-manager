3c52eb8e72ba667e18ca02a1e6f59a7d792befe9
chetan882777 <chetan882777@gmail.com>
2019-06-11 02:27:19 +0530

    logout

--- src/routers/user.js
+++ src/routers/user.js
@@ -15,7 +15,27 @@ router.post('/users', async (req, res) => {
     }
 })
 
+router.post('/logout', auth, async(req, res) => {
+    try{
+        req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token )
+        await req.user.save()
 
+        res.send()
+
+    }catch(e){
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutall', auth, async (req, res) => {
+    try{
+        req.user.tokens = []
+        await user.save()
+        res.send()
+    }catch(e){
+        res.status(500).send()
+    }
+})
 
 router.get('/users/me', auth,  async (req, res) => {
     res.send(req.user)
