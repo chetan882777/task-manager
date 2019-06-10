986669e60c8c306d7df9c918b7b1b1a8ff792ef7
chetan882777 <chetan882777@gmail.com>
2019-06-11 01:59:23 +0530

    Accepting authentication with tokens

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
+const auth = require('../middleware/auth')
 
 
 router.post('/users', async (req, res) => {
@@ -16,13 +17,8 @@ router.post('/users', async (req, res) => {
 
 
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.status(201).send(users)
-    } catch (e) {
-        res.send(500).send(e)
-    }
+router.get('/users/me', auth,  async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
