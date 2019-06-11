dc840cb0002cc4da992d609ca9b88a0556d60d30
chetan882777 <chetan882777@gmail.com>
2019-06-11 23:56:27 +0530

    filtering data

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,10 +18,18 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 router.get('/tasks', auth, async (req, res) => {
-    try {
-        //const tasks = await Task.find({owner: req.user._id})
 
-        await req.user.populate('tasks').execPopulate()
+    const match = {}
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+    try {
+       await req.user.populate(
+            {
+                path: 'tasks',
+                match
+            }).execPopulate()
+
         res.send(req.user.tasks)
     } catch (e) {
         res.status(400).send(e)
@@ -31,7 +39,7 @@ router.get('/tasks', auth, async (req, res) => {
 router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
     try {
-        const task = await Task.findOne({_id, owner: req.user._id})
+        const task = await Task.findOne({ _id, owner: req.user._id })
         if (!task) {
             res.status(404).send()
         }
@@ -51,7 +59,7 @@ router.patch('/tasks/:id', auth, async (req, res) => {
         return res.send({ error: "Invalid update's!" })
     }
     try {
-        const task = await Task.findOne({_id: req.params.id, owner: req.user._id})
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
 
         if (!task) {
             return res.status(404).send()
@@ -68,7 +76,7 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 
 router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findOneAndDelete({_id:req.params.id, owner: req.user._id})
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
         if (!task) {
             return res.status(404).send()
         }
