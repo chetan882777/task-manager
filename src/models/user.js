960f7401e556d2b92a0d4b13d95477a4a5dc3a16
chetan882777 <chetan882777@gmail.com>
2019-06-11 14:39:24 +0530

    hiding private data

--- src/models/user.js
+++ src/models/user.js
@@ -53,6 +53,18 @@ const userSchema = new mongoose.Schema({
     }
 )
 
+userSchema.methods.toJSON = function (){
+    const user = this
+
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+}
+
+
 userSchema.methods.generateAuthToken = async function(){
     const user = this
     const token =  jwt.sign({_id: user._id.toString()}, 'thisismysecret')
