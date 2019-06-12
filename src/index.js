89f483a91600145fbe53eef810464b492188ba36
chetan882777 <chetan882777@gmail.com>
2019-06-13 01:17:18 +0530

    upload a file using multer

--- src/index.js
+++ src/index.js
@@ -11,6 +11,16 @@ app.use(userRouter)
 app.use(taskRouter)
 
 
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
+
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
