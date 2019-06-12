6de94b7c713822434eb39623a055a0ec4e6e1fb3
chetan882777 <chetan882777@gmail.com>
2019-06-13 02:37:06 +0530

    remove avatar from profile results

--- src/index.js
+++ src/index.js
@@ -10,29 +10,6 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-
-const multer = require('multer')
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb){
-        if(!file.originalname.match(/\.(doc|docx)$/)){
-            return cb(new Error('Please upload a Word document'))
-        }
-
-        cb(undefined, true)
-    }
-})
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-},(error, req, res, next) => {
-    res.status(400).send({error: error.message})
-})
-
-
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
--- src/models/user.js
+++ src/models/user.js
@@ -72,6 +72,7 @@ userSchema.methods.toJSON = function (){
 
     delete userObject.password
     delete userObject.tokens
+    delete userObject.avatar
 
     return userObject
 }
