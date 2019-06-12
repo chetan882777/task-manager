d013c9e6edbab879dcd4e68d39e53f6c694950f3
chetan882777 <chetan882777@gmail.com>
2019-06-13 02:24:54 +0530

    Serving up files

--- src/routers/user.js
+++ src/routers/user.js
@@ -113,4 +113,19 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
 
 })
 
+router.get('/users/:id/avatar', async (req, res) =>{
+    try{
+        const user = await User.findById(req.params.id)
+
+        if(!user || !user.avatar){
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+    }catch(e){
+        req.status(404).send()
+    }
+})
+
 module.exports = router
