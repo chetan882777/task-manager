b811ae8f5acf9d8aa79952a995d5df9591e56c13
chetan882777 <chetan882777@gmail.com>
2019-06-13 01:40:33 +0530

    Validating file uploads

--- src/index.js
+++ src/index.js
@@ -13,7 +13,17 @@ app.use(taskRouter)
 
 const multer = require('multer')
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(doc|docx)$/)){
+            return cb(new Error('Please upload a Word document'))
+        }
+
+        cb(undefined, true)
+    }
 })
 
 app.post('/upload', upload.single('upload'), (req, res) => {
